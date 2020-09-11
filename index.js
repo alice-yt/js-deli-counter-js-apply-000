515e2151b4368618a2863900c29785685ed7c545
Alice Tang <tang.ayt@gmail.com>
2020-09-11 01:27:27 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+let katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  for (let i = 1; i <= katzDeliLine.length; i++) {
+    return `Welcome, ${name}. You are number ${i} in line.`; 
+  }
+}
+
+function nowServing(katzDeliLine) {
+    if (katzDeliLine.length === 0) {
+      return 'There is nobody waiting to be served!'
+    } else {
+      while (katzDeliLine.length !== 0) {
+       return katzDeliLine[i];
+       katzDeliLine = katzDeliLine.slice(1); 
+      }
+    }
+  }
+  
+  function currentLine(katzDeliLine) {
+    let str = 'The line is currently: ';
+    if (katzDeliLine.length === 0) {
+      return 'The line is currently empty.'
+    } else {
+      for (let i = 0; i < katzDeliLine.length; i++) {
+        str += `${i + 1}. ${katzDeliLine[i]} `;
+      }
+    }
+  }
