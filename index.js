7b38266a3d722fd12408b7a4dd50f5b961872d35
Alice Tang <tang.ayt@gmail.com>
2020-09-11 01:59:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function nowServing(katzDeliLine) {
       for (let i = 0; i < newLine.length; i++) {
         line += (i + 1) + '. ' + newLine[i] + ', '
       }
-        return 'The line is currently: ' += line;
+        return 'The line is currently: ' + line;
     }
   }
