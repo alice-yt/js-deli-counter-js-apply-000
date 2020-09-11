4fb50d558360a4acfcb1c4a1a7a57c33fee5dff2
Alice Tang <tang.ayt@gmail.com>
2020-09-11 01:48:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
-  }
 }
 
 function nowServing(katzDeliLine) {
